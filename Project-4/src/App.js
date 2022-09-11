dbdbc0f9b11860c1d941f85382f48fc992fc68a4
Jerin5921 <112006704+Jerin5921@users.noreply.github.com>
2022-09-11 21:21:31 +0400

    Add files via upload

---
+++ Project-4/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+import Password from "./components/Password"
+
+function App() {
+  return (
+    <>
+      <section>
+        <div className='title'>
+          <h2>Password Generator </h2>
+        </div>
+        <Password />
+      </section>
+    </>
+  )
+}
+
+export default App
